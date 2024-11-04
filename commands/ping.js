abbe7b3dd46e60b56206aec4e957a82bf2c2709b
いるかぁぁ <104310722+Dolphin-kun@users.noreply.github.com>
2024-11-04 23:33:11 +0900

    きれいにした

--- commands/ping.js
+++ commands/ping.js
@@ -4,9 +4,9 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Ping値を計測します'),
-  
+
 	async execute(client, interaction) {
-    await interaction.reply({ content: `計算中`, ephemeral: true });
+		await interaction.reply({ content: `計算中`, ephemeral: true });
 		await interaction.editReply({ content: `Pong! APIレイテンシ : ${Math.round(client.ws.ping)}ms 🛰️`, ephemeral: true });
 	},
 };
